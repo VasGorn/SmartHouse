bb7e2e56ca67966b58f4c704056c95e643bb1e5e
VasGor <kingstar.gvv@gmail.com>
2018-08-10 16:27:48 +0300

    create fragments for different rooms

---
+++ app/src/main/java/onpu/vasiliy/smarthouse/fragments/Kitchen.java
@@ -0,0 +1,25 @@
+package onpu.vasiliy.smarthouse.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import onpu.vasiliy.smarthouse.R;
+
+public class Kitchen extends Fragment{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kitchen,container,false);
+        //return super.onCreateView(inflater, container, false);
+    }
+}
