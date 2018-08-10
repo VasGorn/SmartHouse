fd60aa888f9fde31163d02e99b5c6c517cd4c348
VasGor <kingstar.gvv@gmail.com>
2018-08-10 17:37:13 +0300

    add fragments to tab layout

--- app/src/main/java/onpu/vasiliy/smarthouse/Control.java
+++ app/src/main/java/onpu/vasiliy/smarthouse/Control.java
@@ -1,13 +1,79 @@
 package onpu.vasiliy.smarthouse;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import onpu.vasiliy.smarthouse.fragments.Bathroom;
+import onpu.vasiliy.smarthouse.fragments.Hall;
+import onpu.vasiliy.smarthouse.fragments.Kitchen;
 
 public class Control extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+
+        //toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tabs);
+
+        //setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setupViewPager(viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Bathroom(), getResources().getString(R.string.bathroom));
+        adapter.addFragment(new Hall(), getResources().getString(R.string.hall));
+        adapter.addFragment(new Kitchen(), getResources().getString(R.string.kitchen));
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager){
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return mFragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
